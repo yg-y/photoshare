c02d1f3a5488d948af335a3ed4445693e80af7a9
young <y627756022@163.com>
2019-04-02 01:49:40 +0800

    create study module

---
+++ photoshare-controller/src/main/java/com/young/photoshare/PhotoShareControllerApplication.java
@@ -0,0 +1,13 @@
+package com.young.photoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoShareControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoShareControllerApplication.class, args);
+    }
+
+}
---
+++ photoshare-controller/src/main/java/com/young/photoshare/config/IndexView.java
@@ -0,0 +1,14 @@
+//package com.young.photoshare.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+//@Configuration
+//public class IndexView extends WebMvcConfigurationSupport {
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("/index.html");
+//    }
+//}
