7c43535da3cea0c8717b49604b7a5f2e52cd2ef9
young <y627756022@163.com>
2019-04-20 23:47:25 +0800

    更新pom架构混乱的问题 添加mybatis plus 支持

--- photoshare-controller/src/main/java/com/young/photoshare/config/JavaMelodyConfiguration.java
+++ photoshare-controller/src/main/java/com/young/photoshare/config/JavaMelodyConfiguration.java
@@ -1,30 +1,30 @@
-package com.young.photoshare.config;
-
-import net.bull.javamelody.MonitoringFilter;
-import net.bull.javamelody.SessionListener;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * Java Melody 应用监控配置类
- */
-@Configuration
-public class JavaMelodyConfiguration {
-
-    @Bean
-    public FilterRegistrationBean monitorFilter() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new MonitoringFilter());
-        filterRegistrationBean.addUrlPatterns("/*");
-        return filterRegistrationBean;
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean sessionListener() {
-        ServletListenerRegistrationBean servletListenerRegistrationBean = new ServletListenerRegistrationBean();
-        servletListenerRegistrationBean.setListener(new SessionListener());
-        return servletListenerRegistrationBean;
-    }
-
-}
+//package com.young.photoshare.config;
+//
+//import net.bull.javamelody.MonitoringFilter;
+//import net.bull.javamelody.SessionListener;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * Java Melody 应用监控配置类
+// */
+//@Configuration
+//public class JavaMelodyConfiguration {
+//
+//    @Bean
+//    public FilterRegistrationBean monitorFilter() {
+//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new MonitoringFilter());
+//        filterRegistrationBean.addUrlPatterns("/*");
+//        return filterRegistrationBean;
+//    }
+//
+//    @Bean
+//    public ServletListenerRegistrationBean sessionListener() {
+//        ServletListenerRegistrationBean servletListenerRegistrationBean = new ServletListenerRegistrationBean();
+//        servletListenerRegistrationBean.setListener(new SessionListener());
+//        return servletListenerRegistrationBean;
+//    }
+//
+//}
