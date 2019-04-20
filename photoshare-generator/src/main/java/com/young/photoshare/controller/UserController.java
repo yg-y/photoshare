b842c5309fd8ed4ba11c8d4075022389ccdfc38e
young <y627756022@163.com>
2019-04-21 01:10:16 +0800

    提交mybatis plus 代码构建工具

---
+++ photoshare-generator/src/main/java/com/young/photoshare/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.young.photoshare.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author young
+ * @since 2019-04-21
+ */
+@RestController
+@RequestMapping("/photoshare/user")
+public class UserController {
+
+}
---
+++ photoshare-generator/src/main/java/com/young/photoshare/entity/User.java
@@ -0,0 +1,54 @@
+package com.young.photoshare.entity;
+
+import java.io.Serializable;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author young
+ * @since 2019-04-21
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户id
+     */
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 邮箱
+     */
+    private String userEmail;
+
+    /**
+     * 手机
+     */
+    private String userPhone;
+
+    /**
+     * 性别【1.男2.女】
+     */
+    private Boolean userSex;
+
+    private String userAddress;
+
+    private String userAutograph;
+
+
+}
