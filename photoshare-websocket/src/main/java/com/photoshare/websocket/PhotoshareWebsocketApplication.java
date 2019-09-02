9bbdfe1feca9882308c38af0cbcc613d069db7d5
young <y627756022@163.com>
2019-09-02 11:19:49 +0800

    上传websocket,待测试

---
+++ photoshare-websocket/src/main/java/com/photoshare/websocket/PhotoshareWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.photoshare.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoshareWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoshareWebsocketApplication.class, args);
+    }
+
+}
