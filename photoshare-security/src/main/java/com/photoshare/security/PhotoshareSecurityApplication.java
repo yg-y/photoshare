70b3d81175addf5d09a6ea65cdbd535efe82302f
young <y627756022@163.com>
2019-09-19 18:18:29 +0800

    update security oauth

---
+++ photoshare-security/src/main/java/com/photoshare/security/PhotoshareSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.photoshare.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoshareSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoshareSecurityApplication.class, args);
+    }
+
+}
