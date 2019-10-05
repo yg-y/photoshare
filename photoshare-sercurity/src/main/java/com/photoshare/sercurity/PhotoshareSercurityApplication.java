eaf6b6b90677b3e642e4fe0d95960f1caa2991ea
young <y627756022@163.com>
2019-10-06 00:07:30 +0800

    upload spring security demo test

---
+++ photoshare-sercurity/src/main/java/com/photoshare/sercurity/PhotoshareSercurityApplication.java
@@ -0,0 +1,13 @@
+package com.photoshare.sercurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoshareSercurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoshareSercurityApplication.class, args);
+    }
+
+}
