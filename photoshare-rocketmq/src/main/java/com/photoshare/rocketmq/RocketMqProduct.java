8d10e6dfd9b5d6acac4562c6e1df0550aa41c3a7
young <y627756022@163.com>
2020-04-14 16:32:13 +0800

    提交 rocket mq 简单示例

--- photoshare-rocketmq/src/main/java/com/photoshare/rocketmq/RocketMqProduct.java
+++ photoshare-rocketmq/src/main/java/com/photoshare/rocketmq/RocketMqProduct.java
@@ -1,20 +1,14 @@
 package com.photoshare.rocketmq;
 
-import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 
 import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 
 public class RocketMqProduct {
