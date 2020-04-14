5b4cfa4c52d5cab169ec220948eb6a5aa029a64e
young <y627756022@163.com>
2020-04-14 16:10:14 +0800

    提交 rocket mq 简单事列

---
+++ photoshare-rocketmq/src/main/java/com/photoshare/rocketmq/RocketMqConsumer.java
@@ -0,0 +1,61 @@
+package com.photoshare.rocketmq;
+
+import lombok.SneakyThrows;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+
+public class RocketMqConsumer {
+    public static void main(String[] args) throws Exception {
+        RocketMqConsumer rocketMqClient = new RocketMqConsumer();
+
+        System.err.println("-------------正在消费消息--------------");
+        rocketMqClient.consumerMq();
+        System.err.println("-------------结束消费消息--------------");
+    }
+
+    /**
+     * 监听方式 监听到有消息就消费
+     *
+     * @throws MQClientException
+     */
+    public void consumerMq() throws MQClientException {
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("YOUNG_GROUP");
+
+        // Specify name server addresses.
+        consumer.setNamesrvAddr("localhost:9876");
+
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("YOUNG_TEST_MQ", "*");
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @SneakyThrows
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), new String(msgs.get(0).getBody(), "UTF-8"));
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ photoshare-rocketmq/src/main/java/com/photoshare/rocketmq/RocketMqProduct.java
@@ -0,0 +1,53 @@
+package com.photoshare.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+
+public class RocketMqProduct {
+    public static void main(String[] args) throws Exception {
+        RocketMqProduct rocketMqClient = new RocketMqProduct();
+
+        System.err.println("-------------正在生产消息--------------");
+        rocketMqClient.productMq();
+        System.err.println("-------------生产消息结束--------------");
+    }
+
+    public void productMq() throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException, MQBrokerException {
+
+//        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("YOUNG_GROUP");
+//        // Specify name server addresses.
+        producer.setNamesrvAddr("localhost:9876");
+//        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(5000);
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("YOUNG_TEST_MQ" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+//        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
